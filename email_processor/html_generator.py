f1f66330ad4f1e8146839c550c8cfee1c92bbf7f
guimig <82185508+guimig@users.noreply.github.com>
2025-02-22 00:13:07 -0300

    Update html_generator.py

--- email_processor/html_generator.py
+++ email_processor/html_generator.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from email_processor.config import Config
 
-# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# Configuração do logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class HtmlGenerator:
     HTML_TEMPLATE = """
@@ -154,10 +155,16 @@ class HtmlGenerator:
             normalized_title = os.path.basename(root)
             output_path = os.path.join(os.getcwd(), f"{normalized_title}.html")
 
+            # Lê e limpa o conteúdo HTML
             with open(latest_file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
-                content = BeautifulSoup(content, 'html.parser').prettify()  # Limpa o HTML
+                content = self._clean_html(content)  # Limpa o HTML
 
+            # Verifica se há strings problemáticas
+            if 'font-family' in content:
+                logging.warning(f"Arquivo {latest_file} contém 'font-family'. Verifique o conteúdo.")
+
+            # Extrai a data do nome do arquivo
             match = re.search(r'(\d{2})-(\d{2})-(\d{4})\.html', latest_file)
             if match:
                 day, month, year = map(int, match.groups())
@@ -165,15 +172,25 @@ class HtmlGenerator:
             else:
                 last_report_date = datetime.fromtimestamp(os.path.getmtime(latest_file_path), Config.TIMEZONE)
 
+            # Adiciona a data e hora da última atualização no final do HTML
             now = datetime.now(Config.TIMEZONE)
             update_text = f"<p>Última atualização: {now.strftime('%d/%m/%Y %H:%M:%S')}</p>"
             report_date_text = f"<p>Data do relatório: {last_report_date.strftime('%d/%m/%Y')}</p>"
 
+            # Escreve o conteúdo atualizado no arquivo de saída
             with open(output_path, 'w', encoding='utf-8') as f:
                 f.write(content + report_date_text + update_text)
+        except UnicodeDecodeError as e:
+            logging.error(f"Erro de decodificação no arquivo {latest_file}: {e}")
         except Exception as e:
             logging.error(f"Erro ao processar o arquivo {latest_file}: {e}")
 
+    def _clean_html(self, content):
+        """
+        Limpa o conteúdo HTML usando BeautifulSoup.
+        """
+        soup = BeautifulSoup(content, 'html.parser')
+        return soup.prettify()
 
     def _collect_links(self):
         root_links = []
