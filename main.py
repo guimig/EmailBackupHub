dd7ff2bc4065c947bc6b9c368b093bffedea3223
guimig <82185508+guimig@users.noreply.github.com>
2025-02-22 11:54:54 -0300

    atualiza main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from email_processor import process_emails
-from html_generator import create_latest_summary_html, update_root_index
+#from html_generator2 import create_latest_summary_html, update_root_index
+from html_generator import update_root_index
 
 if __name__ == '__main__':
     try:
         process_emails()
-        create_latest_summary_html()
+        #create_latest_summary_html()
         update_root_index()
     except Exception as e:
         print(f"Erro ao executar o script: {e}")
