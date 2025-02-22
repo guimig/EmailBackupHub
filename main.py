e6f1220bcf283182b4b12286aa043b649ec4464b
guimig <82185508+guimig@users.noreply.github.com>
2025-02-22 12:11:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from email_processor import process_emails
-#from html_generator2 import create_latest_summary_html, update_root_index
-from html_generator import update_root_index
+from html_generator import create_latest_summary_html, update_root_index
 
 if __name__ == '__main__':
     try:
+        # 1. Processa novos e-mails e gera arquivos nas pastas
         process_emails()
-        #create_latest_summary_html()
+        
+        # 2. Cria arquivos .html na raiz com os últimos relatórios de cada pasta
+        create_latest_summary_html()
+        
+        # 3. Atualiza o index.html com todos os links
         update_root_index()
+        
     except Exception as e:
-        print(f"Erro ao executar o script: {e}")
+        print(f"Erro no processo principal: {e}")
